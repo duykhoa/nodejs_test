1d992b65d91db4008ba8c7d4d9b549311580e3a0
Duy Khoa <duykhoa12t@gmail.com>
2016-12-30 00:23:34 +0800

    add checkAvailable route

--- server/main.js
+++ server/main.js
@@ -2,56 +2,77 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 var CheckAvailable = require('./check_available.js');
+const Availabilities = new Mongo.Collection('availabilities');
 
 Meteor.startup(() => {
-  Router.route('/check-available', {where: 'server'})
+  function timeToInt(time_string) {
+    matched = /(\d+):(\d+)(\w+)/.exec(time_string);
+    time_num = 0;
+
+    if (matched) {
+      hours = matched[1];
+
+      if (hours == '12') {
+        hours = '0'
+      }
+
+      time_num = parseInt(hours) * 3600 + parseInt(matched[2]);
+
+      if (matched[3] == 'pm' || matched[3] == 'PM') {
+        time_num = time_num +  12 * 3600;
+      }
+    }
+
+    return time_num;
+  }
+
+  Router.route('/check_available', {where: 'server'})
     .post(function() {
       var checkAvailable = new CheckAvailable();
+      requestObj = this.request.body;
 
-      userId = this.params.userId;
-      availableArgs = this.params.availableArgs;
+      var userId = requestObj.userId;
+      var start_date = requestObj.start_date;
+      var end_date = requestObj.end_date;
+      var start_time = requestObj.start_time;
+      var end_time = requestObj.end_time;
+      var days = requestObj.days.split(",");
 
-      responseJson = {};
+      var availableArgs = {
+        start_time: start_time,
+        end_time: end_time,
+        start_date: start_date,
+        end_date: end_date,
+        days: days
+      }
+
+      availabilitiesJson = Availabilities.find({user_id: userId.toString()}).fetch();
+
+      responseJson = checkAvailable.checkAvailable(availabilitiesJson, availableArgs);
+
       this.response.setHeader('Content-Type','application/json');
       this.response.end(JSON.stringify({"status": 200, response: responseJson}));
     })
 
   Router.route('/seed', {where: 'server'})
     .get(function() {
-      function timeToInt(time_string) {
-        matched = /(\d+):(\d+)(\w+)/.exec(time_string);
-        time_num = 0;
-
-        if (matched) {
-          hours = matched[1];
-
-          if (hours == '12') {
-            hours = '0'
-          }
-
-          time_num = parseInt(hours) * 3600 + parseInt(matched[2]);
-
-          if (matched[3] == 'pm' || matched[3] == 'PM') {
-            time_num = time_num +  12 * 3600;
-          }
-        }
-
-        return time_num;
-      }
-
-      Availabilities = new Mongo.Collection('availabilities');
       Users = new Mongo.Collection('users');
 
       var availabilitiesJson = require('../db/user_availabilities.json');
       var usersJson = require('../db/users.json');
 
       availabilitiesJson.forEach(function(element) {
-        element.start_time = timeToInt(element.start_time);
-        element.end_time = timeToInt(element.end_time);
-        if (element.event_type == "Timeoff") {
-          element.start_date = new Date(Date.parse(element.start_date));
-          element.end_date = new Date(Date.parse(element.end_date));
-        }
+        //element.start_time = timeToInt(element.start_time);
+        //element.end_time = timeToInt(element.end_time);
+
+        //if (element.end_time < element.start_time) {
+          //element.end_time = element.end_time + 12 * 3600;
+        //}
+
+        //if (element.event_type == "Timeoff") {
+          //element.start_date = new Date(Date.parse(element.start_date));
+          //element.end_date = new Date(Date.parse(element.end_date));
+        //}
 
         Availabilities.insert(element);
       })
